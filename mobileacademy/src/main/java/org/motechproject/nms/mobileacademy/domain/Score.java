1632193fccf42a76eda06f53bdb5b57f9c26f6ed
kanchan nms <kanchannms@gmail.com>
2015-02-20 10:42:00 +0530

    NMS-16 change in score.java

--- mobileacademy/src/main/java/org/motechproject/nms/mobileacademy/domain/Score.java
+++ mobileacademy/src/main/java/org/motechproject/nms/mobileacademy/domain/Score.java
@@ -2,8 +2,7 @@ package org.motechproject.nms.mobileacademy.domain;
 
 import org.motechproject.mds.annotations.Entity;
 import org.motechproject.mds.annotations.Field;
-import org.motechproject.mds.domain.MdsEntity;
-import java.lang.String;
+import org.motechproject.mds.domain.MdsEntity;import java.lang.String;
 
 @Entity
 public class Score extends MdsEntity {
