b072442e3872988c9a568b48b24cdf3c84ebaf0f
mallika <mallikanms2015@gmail.com>
2015-02-27 15:19:02 +0530

    NMS-12 Updated error description constants
    Updated description for erroneous records to be logged

--- util/src/main/java/org/motechproject/nms/util/constants/ErrorDescriptionConstants.java
+++ util/src/main/java/org/motechproject/nms/util/constants/ErrorDescriptionConstants.java
@@ -8,9 +8,9 @@ package org.motechproject.nms.util.constants;
  */
 public final class ErrorDescriptionConstants {
 
-    public static final String MANDATORY_PARAMETER_MISSING_DESCRIPTION = "Upload unsuccessful. %s missing.";
-    public static final String INVALID_DATA_DESCRIPTION = "Upload unsuccessful. Invalid %s";
-    public static final String CSV_RECORD_MISSING_DESCRIPTION = "Record Not Found in CSV";
+    public static final String MANDATORY_PARAMETER_MISSING_DESCRIPTION = "Upload unsuccessful as %s is missing.";
+    public static final String INVALID_DATA_DESCRIPTION = "Upload unsuccessful as %s invalid.";
+    public static final String CSV_RECORD_MISSING_DESCRIPTION = "Upload unsuccessful as record is missing in CSV.";
 
     private ErrorDescriptionConstants() {
 
