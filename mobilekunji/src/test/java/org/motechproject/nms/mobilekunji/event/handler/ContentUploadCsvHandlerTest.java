78024a0d5386c6bfce57f273c8ae21fc866575ac
srivastavaAbhi <abhisrivastava.17016@gmail.com>
2015-03-02 16:51:57 +0530

    NMS-18 MK test file added

---
+++ mobilekunji/src/test/java/org/motechproject/nms/mobilekunji/event/handler/ContentUploadCsvHandlerTest.java
@@ -0,0 +1,39 @@
+package org.motechproject.nms.mobilekunji.event.handler;
+
+import org.junit.Test;
+
+public class ContentUploadCsvHandlerTest {
+
+
+
+
+    @Test
+    public void testMobileKunjiContentUploadSuccess() throws Exception {
+
+    }
+
+    @Test
+    public void testMobileKunjiContentUploadCsvFailure() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFailCount() throws Exception {
+
+    }
+
+    @Test
+    public void testSetFailCount() throws Exception {
+
+    }
+
+    @Test
+    public void testGetSuccessCount() throws Exception {
+
+    }
+
+    @Test
+    public void testSetSuccessCount() throws Exception {
+
+    }
+}
---
+++ mobilekunji/src/test/java/org/motechproject/nms/mobilekunji/service/impl/ConfigurationServiceImplTest.java
@@ -0,0 +1,21 @@
+package org.motechproject.nms.mobilekunji.service.impl;
+
+import org.junit.Test;
+
+public class ConfigurationServiceImplTest {
+
+    @Test
+    public void testCreateConfiguration() throws Exception {
+
+    }
+
+    @Test
+    public void testGetConfiguration() throws Exception {
+
+    }
+
+    @Test
+    public void testIsConfigurationPresent() throws Exception {
+
+    }
+}
