21efb7c26d039b905de60ea5682384990a71bc5f
mallika <mallikanms2015@gmail.com>
2015-03-18 11:23:34 +0530

    Removing unused constants from Utils.

--- util/src/main/java/org/motechproject/nms/util/constants/Constants.java
+++ util/src/main/java/org/motechproject/nms/util/constants/Constants.java
@@ -6,15 +6,6 @@ package org.motechproject.nms.util.constants;
  */
 public final class Constants {
 
-    public static final String NEXT_LINE = "\n";
-    public static final String TAB = "\t";
-    public static final String DELIMITER = ", ";
-    public static final String ERROR_LOG_TITLE = "Record upload failed for : ";
-    public static final String BULK_UPLOAD_SUMMARY_TITLE = "Bulk Upload Summary : ";
-    public static final String UNDERLINE = "------------------------------------------------------------------------------------------------------------------";
-    public static final String USER_NAME_TITLE = "Uploaded by : ";
-    public static final String SUCCESSFUL_RECORDS_TITLE = "Number Of Records Successful : ";
-    public static final String FAILED_RECORDS_TITLE = "Number Of Records Failed : ";
     public static final String EMPTY_STRING = "";
 
     private Constants() {
