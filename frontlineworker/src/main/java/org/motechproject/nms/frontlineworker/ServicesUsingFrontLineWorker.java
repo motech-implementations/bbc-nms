1a97e91f0bcdc4244a2661e0c151b10cfc20932b
abhisheksrivastava <abhisrivastava.17016@gmail.com>
2015-03-20 18:16:11 +0530

    NMS-55 implementing APIs
    implementing APIs

---
+++ frontlineworker/src/main/java/org/motechproject/nms/frontlineworker/ServicesUsingFrontLineWorker.java
@@ -0,0 +1,9 @@
+package org.motechproject.nms.frontlineworker;
+
+/**
+ * This enum specifies the possible values for the services that use front line worker services
+ */
+public enum ServicesUsingFrontLineWorker {
+
+    MOBILEKUNJI, MOBILEACADEMY;
+}
