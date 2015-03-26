791683079a0f83a61056634d7bddb9321d87584b
abhisheksrivastava <abhisrivastava.17016@gmail.com>
2015-03-26 12:47:18 +0530

    NMS -8 incorporating comments
    incorporating comments

--- frontlineworker/src/main/java/org/motechproject/nms/frontlineworker/event/handler/FrontLineWorkerUploadHandler.java
+++ frontlineworker/src/main/java/org/motechproject/nms/frontlineworker/event/handler/FrontLineWorkerUploadHandler.java
@@ -248,10 +248,7 @@ public class FrontLineWorkerUploadHandler {
         dbRecord.setAdhaarNumber(frontLineWorker.getAdhaarNumber());
         dbRecord.setAshaNumber(frontLineWorker.getAshaNumber());
 
-        dbRecord.setCreator(frontLineWorker.getCreator());
-        dbRecord.setModificationDate(frontLineWorker.getModificationDate());
         dbRecord.setModifiedBy(frontLineWorker.getModifiedBy());
-        dbRecord.setCreationDate(frontLineWorker.getCreationDate());
         dbRecord.setOwner(frontLineWorker.getOwner());
         frontLineWorkerService.updateFrontLineWorker(dbRecord);
     }
@@ -342,8 +339,6 @@ public class FrontLineWorkerUploadHandler {
         frontLineWorker.setCreator(record.getCreator());
         frontLineWorker.setModifiedBy(record.getModifiedBy());
         frontLineWorker.setOwner(record.getOwner());
-        frontLineWorker.setModificationDate(record.getModificationDate());
-        frontLineWorker.setCreationDate(record.getCreationDate());
 
         logger.debug("mapFrontLineWorkerFrom process end");
     }
