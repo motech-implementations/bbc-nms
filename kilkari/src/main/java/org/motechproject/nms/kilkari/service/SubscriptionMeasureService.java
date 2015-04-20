64e07bc749b8316c17a43dac7ea7ea143fc0b9a0
DeepakTiwari <deepaktiwarinms@gmail.com>
2015-04-20 11:17:08 +0530

    NMS Added Delection functionality of Subscription after 6 weeks.

---
+++ kilkari/src/main/java/org/motechproject/nms/kilkari/service/SubscriptionMeasureService.java
@@ -0,0 +1,18 @@
+package org.motechproject.nms.kilkari.service;
+
+import org.motechproject.nms.kilkari.domain.SubscriptionMeasure;
+
+
+/**
+ *This interface provides methods to perform crud operations on SubscriptionMeasure object
+ */
+public interface SubscriptionMeasureService {
+
+    /**
+     * This method creates SubscriptionMeasure
+     * @param subscriptionMeasure SubscriptionMeasure type object
+     * @return SubscriptionMeasure type object
+     */
+    SubscriptionMeasure create(SubscriptionMeasure subscriptionMeasure);
+    
+}
