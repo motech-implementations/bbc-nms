1825f38ed7d3d5c12e974f4ec460a412452b143e
abhisheksrivastava <abhisrivastava.17016@gmail.com>
2015-04-22 19:21:49 +0530

    Adding scheduler functionality

--- frontlineworker/src/main/java/org/motechproject/nms/frontlineworker/scheduler/RunScheduler.java
+++ frontlineworker/src/main/java/org/motechproject/nms/frontlineworker/scheduler/RunScheduler.java
@@ -1,15 +1,45 @@
 package org.motechproject.nms.frontlineworker.scheduler;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.motechproject.scheduler.service.MotechSchedulerService;
+
+import java.util.Date;
+
+
 /**
  * Created by abhishek on 21/4/15.
  */
 public class RunScheduler {
 
+    @Autowired
+    private JobLauncher jobLauncher;
 
     @Autowired
-    MotechSchedulerService motechSchedulerService;
+    private Job job;
+
+
+    public void run() {
+
+        try {
+
+            String dateParam = new Date().toString();
+            JobParameters param =
+                    new JobParametersBuilder().addString("date", dateParam).toJobParameters();
+
+            System.out.println(dateParam);
+
+            JobExecution execution = jobLauncher.run(job, param);
+            System.out.println("Exit Status : " + execution.getStatus());
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 
 
 
