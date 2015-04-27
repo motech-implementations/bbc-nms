7d101f1f3d4cb2fadd0f816b60d0993b728965d7
abhisheksrivastava <abhisrivastava.17016@gmail.com>
2015-04-27 17:31:18 +0530

    Adding WhitelistUsersIT in test suite

--- frontlineworker/src/test/java/org/motechproject/nms/frontlineworker/it/suite/IntegrationTests.java
+++ frontlineworker/src/test/java/org/motechproject/nms/frontlineworker/it/suite/IntegrationTests.java
@@ -4,12 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.motechproject.nms.frontlineworker.it.event.handler.FrontlineWorkerHandlerIT;
 import org.motechproject.nms.frontlineworker.it.event.handler.UserProfileDetailsImplIT;
+import org.motechproject.nms.frontlineworker.it.event.handler.WhiteListUsersHandlerIT;
 
 /**
  * Parent IT class to run all the individual IT cases.
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ FrontlineWorkerHandlerIT.class,
-        UserProfileDetailsImplIT.class })
+        UserProfileDetailsImplIT.class, WhiteListUsersHandlerIT.class})
 public class IntegrationTests {
 }
